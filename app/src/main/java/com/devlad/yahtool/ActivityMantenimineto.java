aacbedf64e8acbdbb6c3026941beb90b8dc7b7d2
denysVlad <denisvasquezramos@gmail.com>
2018-05-14 16:56:57 -0600

    actualizacion

--- app/src/main/java/com/devlad/yahtool/ActivityMantenimineto.java
+++ app/src/main/java/com/devlad/yahtool/ActivityMantenimineto.java
@@ -1,11 +1,14 @@
 package com.devlad.yahtool;
 
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.text.SimpleDateFormat;
@@ -38,7 +41,7 @@ public class ActivityMantenimineto extends AppCompatActivity {
 
 
         dataAdapter
-                .setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                .setDropDownViewResource(android.R.layout.simple_spinner_item);
 
 
         spinner.setAdapter(dataAdapter);
@@ -47,6 +50,16 @@ public class ActivityMantenimineto extends AppCompatActivity {
         edit8 = (EditText)findViewById(R.id.editText8);
         edit8.setText(currentDate.format(todayDate));
 
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                ((TextView) parent.getChildAt(0)).setTextColor(Color.WHITE); /* if you want your item to be white */
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+            }
+        });
     }
     public void guardarMante(View v)
     {
